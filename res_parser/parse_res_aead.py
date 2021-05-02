aea085aa68b26d43b4dcf9e13fad10594bd115d5
brailovskii <brailovskii@gmail.com>
2021-05-02 11:27:41 +0300

    standart commit with standart comment

--- res_parser/parse_res_aead.py
+++ res_parser/parse_res_aead.py
@@ -1,21 +1,18 @@
 import os
 import time
 import sys
-
 import re
 
-
-
 def printRes(*args, **kwargs):
-    with open('res.csv','a') as file:
+    with open('res_aead.csv','a') as file:
         print(*args, **kwargs, file=file)
 
 
+empty_prj_text_size = { "O0" : 16360, "O1" : 15808,  "O2" : 15804,  "O3" : 15820,  "Os" : 15772, "Ofast" : 15820 }
 
-results_fn = "capture.txt"
+results_fn = "stm32f303vc_aead_hash_benchmark.txt"
 
 res_enc_dec_str = "type	algorithm	optimization	text	data	bss	enc(0,0)	dec(0,0)	enc(0,8)	dec(0,8)	enc(0,16)	dec(0,16)	enc(0,24)	dec(0,24)	enc(0,32)	dec(0,32)	enc(8,0)	dec(8,0)	enc(8,8)	dec(8,8)	enc(8,16)	dec(8,16)	enc(8,24)	dec(8,24)	enc(8,32)	dec(8,32)	enc(16,0)	dec(16,0)	enc(16,8)	dec(16,8)	enc(16,16)	dec(16,16)	enc(16,24)	dec(16,24)	enc(16,32)	dec(16,32)	enc(24,0)	dec(24,0)	enc(24,8)	dec(24,8)	enc(24,16)	dec(24,16)	enc(24,24)	dec(24,24)	enc(24,32)	dec(24,32)	enc(32,0)	dec(32,0)	enc(32,8)	dec(32,8)	enc(32,16)	dec(32,16)	enc(32,24)	dec(32,24)	enc(32,32)	dec(32,32)"
-
 
 print("parsing results")
 
@@ -25,7 +22,6 @@ results = f.read()
 f.close()
 
 lines = results.splitlines()
-
 printRes(res_enc_dec_str)
 
 for line in lines[:]:
@@ -33,8 +29,8 @@ for line in lines[:]:
         #print(line)
         numbers = re.findall(r'\b\d+\b', line)
         words = line.split()
-        print(numbers)
-        print(words)
+        #print(numbers)
+        #print(words)
         
         lst = numbers[10:]
         enc_dec_results = [lst[i] for i in range(len(lst)) if i%3 == 0]
@@ -43,7 +39,12 @@ for line in lines[:]:
         printRes(words[10], end = "\t")#Algorithm name
         printRes(words[8], end = "\t")#optimization
         
-        printRes(numbers[5], end = "\t")#test
+        
+        text_sec_size = int(numbers[5]) - int(empty_prj_text_size[words[8]])
+        
+        print("text section for " + words[8] + " before " + numbers[5], " after " + str(text_sec_size))
+        
+        printRes(text_sec_size, end = "\t")#text
         printRes(numbers[6], end = "\t")#data
         printRes(numbers[7], end = "\t")#bss
         
@@ -52,9 +53,8 @@ for line in lines[:]:
         
         printRes("")
         
-        print(enc_dec_results)
+        #print(enc_dec_results)
         print("\n\n")
         
-
 
 wait_for_keyboard = input("Exiting")
--- res_parser/parse_res_hash.py
+++ res_parser/parse_res_hash.py
@@ -1,21 +1,18 @@
 import os
 import time
 import sys
-
 import re
-
-
 
 def printRes(*args, **kwargs):
     with open('res_hash.csv','a') as file:
         print(*args, **kwargs, file=file)
 
 
+empty_prj_text_size = { "O0" : 16176, "O1" : 15740,  "O2" : 15732,  "O3" : 15748,  "Os" : 15696, "Ofast" : 15748 }
 
-results_fn = "capture.txt"
 
+results_fn = "stm32f303vc_aead_hash_benchmark.txt"
 res_hash_str = "type	algorithm	optimization	text	data	bss	h(0)	h(8)	h(16)	h(32)	h(64)	h(128)	h(256)	h(512)	h(1024)"
-
 
 print("parsing results")
 
@@ -25,7 +22,6 @@ results = f.read()
 f.close()
 
 lines = results.splitlines()
-
 printRes(res_hash_str)
 
 for line in lines[:]:
@@ -43,7 +39,10 @@ for line in lines[:]:
         printRes(words[10], end = "\t")#Algorithm name
         printRes(words[8], end = "\t")#optimization
         
-        printRes(numbers[5], end = "\t")#text
+        text_sec_size = int(numbers[5]) - int(empty_prj_text_size[words[8]])
+        print("text section for " + words[8] + " before " + numbers[5], " after " + str(text_sec_size))
+        
+        printRes(text_sec_size, end = "\t")#text
         printRes(numbers[6], end = "\t")#data
         printRes(numbers[7], end = "\t")#bss
         
@@ -55,6 +54,5 @@ for line in lines[:]:
         print(enc_dec_results)
         print("\n\n")
         
-
 
 wait_for_keyboard = input("Exiting")
